5172419b7eec21a23f395f72e9212414fb867bb6
ntaztonny <atozpp@yahoo.com>
2021-04-03 12:08:50 +0000

    HTML&CSS

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal;
+    var rate = document.getElementById("rate")
+    var years = document.getElementById("selectedyears")
+    var interest = principal * years * rate/100;
+
+    var futureYear = years.value + Date().getFullYear();
+    var result = document.getElementById("result");
+    result.innerHTML;
 }
+
+function setSliderValue()
+{
+    var slider = document.getElementById("rate");
+    var valuedisplay = document.getElementById("rateValue").innerHTML;
+    valuedisplay.innerHTML = slider.value;
+}
+
+
         
